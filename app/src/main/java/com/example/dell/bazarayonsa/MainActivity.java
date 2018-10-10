6e19214b268a0ccc4603f3f4bf99d0913fb990d8
Dell <ra.ayonsicaeros@ugto.mx>
2018-10-10 10:45:37 -0500

    Agregué segunda actividad para la opción de Omitir Sesión

--- app/src/main/java/com/example/dell/bazarayonsa/MainActivity.java
+++ app/src/main/java/com/example/dell/bazarayonsa/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.dell.bazarayonsa;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,11 +14,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d("1", "ejemlplo sincronizar");
-
-        Log.d("1", "ejemlplo sincronizar Rubiiii");
-
-        Log.d("1", "ejemlplo sincronizar final");
+        Button button=findViewById(R.id.omitir_sesion);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),Main2Activity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
